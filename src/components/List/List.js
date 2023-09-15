ef8e038b72dcd46b99f39e482db8b23fc5cac6d5
Dominik Wilk <mr.wilkd@gmail.com>
2023-09-15 09:31:52 +0100

    Create ReactApp

---
+++ src/components/List/List.js
@@ -0,0 +1,43 @@
+import Column from '../Column/Column';
+import styles from './List.module.scss';
+import { useState } from 'react';
+import shortid from 'shortid';
+
+const List = () => {
+  const [columns, setColumns] = useState([
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' },
+  ]);
+
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
+
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do<span>soon</span>
+        </h2>
+      </header>
+      <p className={styles.description}>Intresting things I want to check out</p>
+      <section className={styles.columns}>
+        {columns.map(column => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
+        ;
+      </section>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={value} onChange={e => setValue(e.target.value)} />
+        <button>Add column</button>
+      </form>
+    </div>
+  );
+};
+
+export default List;
