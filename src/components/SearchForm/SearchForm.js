0204da0215a35ed37c2a7e57a7ad7e4f44304596
Dominik Wilk <mr.wilkd@gmail.com>
2023-09-29 14:04:12 +0100

    Add function to allow search cards

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,28 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+  const [searchString, setSearchString] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleInputChange = e => {
+    setSearchString(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+    setSearchString('');
+  };
+
   return (
-    <form className={styles.searchForm}>
-      <TextInput placeholder='Search…' />
-      <Button>
+    <form onSubmit={handleSubmit} className={styles.searchForm}>
+      <TextInput placeholder='Search…' onChange={handleInputChange} value={searchString} />
+      <Button type='submit'>
         <span className='fa fa-search' />
       </Button>
     </form>
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import initialState from './initialState';
 
 const reducer = (state, action) => {
   if (action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.payload] };
+
   if (action.type === 'ADD_CARD') return { ...state, cards: [...state.cards, action.payload] };
+
+  if (action.type === 'UPDATE_SEARCHSTRING') return { ...state, searchString: action.payload };
+
   return state;
 };
 
