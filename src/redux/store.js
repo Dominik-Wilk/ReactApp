730e41fdbde7f61ba5a56dcce0432777a488486a
Dominik Wilk <mr.wilkd@gmail.com>
2023-09-29 14:09:42 +0100

    Use 'switch statement' instead of 'if statement'

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,19 @@ import { createStore } from 'redux';
 import initialState from './initialState';
 
 const reducer = (state, action) => {
-  if (action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.payload] };
+  switch (action.type) {
+    case 'ADD_COLUMN':
+      return { ...state, columns: [...state.columns, action.payload] };
 
-  if (action.type === 'ADD_CARD') return { ...state, cards: [...state.cards, action.payload] };
+    case 'ADD_CARD':
+      return { ...state, cards: [...state.cards, action.payload] };
 
-  if (action.type === 'UPDATE_SEARCHSTRING') return { ...state, searchString: action.payload };
+    case 'UPDATE_SEARCHSTRING':
+      return { ...state, searchString: action.payload };
 
-  return state;
+    default:
+      return state;
+  }
 };
 
 const store = createStore(
