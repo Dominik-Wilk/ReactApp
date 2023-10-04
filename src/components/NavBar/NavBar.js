32d1b108950d016dc93d410f4e557ff349881905
Dominik Wilk <mr.wilkd@gmail.com>
2023-10-04 14:45:12 +0100

    Add NavLink components to navigation & change color for active link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,41 @@
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div className={styles.nav}>
       <span className={styles.logo}>
-        <a href='/'>
+        <Link to='/'>
           <i className='fa fa-tasks'></i>
-        </a>
+        </Link>
       </span>
       <ul className={styles.list}>
         <li className={styles.link}>
-          <a href='/'>Home</a>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.linkActive : undefined
+            }
+            to='/'>
+            Home
+          </NavLink>
         </li>
         <li className={styles.link}>
-          <a href='/about'>About</a>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.linkActive : undefined
+            }
+            to='/about'>
+            About
+          </NavLink>
         </li>
         <li className={styles.link}>
-          <a href='/favourite'>Favourite</a>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.linkActive : undefined
+            }
+            to='/favourite'>
+            Favourite
+          </NavLink>
         </li>
       </ul>
     </div>
