dfed8d069e909213c0dc80525aa6f70d0033879e
Dominik Wilk <mr.wilkd@gmail.com>
2023-10-05 13:08:59 +0100

    Add a function that allows us to add a new list to the collection of lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
+import List from './components/List/List.js';
 import About from './components/About/About.js';
 import Favourite from './components/Favourite/Favourite.js';
 import { Routes, Route } from 'react-router-dom';
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/favourite' element={<Favourite />} />
+          <Route path='/list/:listId' element={<List />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </Container>
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,13 +9,24 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
   );
 
 export const getAllColumns = state => state.columns;
+export const getAllLists = state => state.lists;
+
+export const getListById = ({ lists }, listId) => {
+  return lists.find(list => list.id === listId);
+};
+export const getColumnsByList = ({ columns }, listId) => {
+  return columns.filter(column => column.listId === listId);
+};
 
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
+export const addList = payload => ({ type: 'ADD_LIST', payload });
 export const searchPhrase = payload => ({
   type: 'UPDATE_SEARCHSTRING',
   payload,
 });
+
+export const stateString = state => state.searchString;
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -34,6 +45,12 @@ const reducer = (state, action) => {
     case 'UPDATE_SEARCHSTRING':
       return { ...state, searchString: action.payload };
 
+    case 'ADD_LIST':
+      return {
+        ...state,
+        lists: [...state.lists, { ...action.payload, id: nanoid() }],
+      };
+
     default:
       return state;
   }
@@ -46,11 +63,5 @@ const store = configureStore({
     : f => f,
   preloadedState: initialState,
 });
-
-// const store = configureStore(
-//   reducer,
-//   initialState,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-// );
 
 export default store;
